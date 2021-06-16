13a3504146444f4c00a2efb69ed6d82784309caa
Malihatmalam <malihatmalam@gmail.com>
2021-06-16 15:41:03 +0700

    Fix Error 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
-import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
 
 import Router from "./router";
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,10 @@
 import { lazy } from "react";
 import IntroContent from "../../content/IntroContent.json";
-import MiddleBlockContent from "../../content/MiddleBlockContent.json";
-import AboutContent from "../../content/AboutContent.json";
 import OutputContent from "../../content/OutputContent.json";
 import DeliveyContent from "../../content/DeliveryContent.json";
-import MissionContent from "../../content/MissionContent.json";
-import ProductContent from "../../content/ProductContent.json";
 import ContactContent from "../../content/ContactContent.json";
 
 const Contact = lazy(() => import("../../components/ContactForm"));
-const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
 const Container = lazy(() => import("../../common/Container"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 const ContentBlock = lazy(() => import("../../components/ContentBlock"));
@@ -26,11 +21,6 @@ const Home = () => {
         icon="intro.svg"
         id="intro"
       />
-      {/* <MiddleBlock
-        title={MiddleBlockContent.title}
-        content={MiddleBlockContent.text}
-        button={MiddleBlockContent.button}
-      /> */}
       <ContentBlock
         type="left"
         title={OutputContent.title}
